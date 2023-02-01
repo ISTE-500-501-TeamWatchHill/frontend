d8f6059696b98a474b781fff468627a00b99c568
vsoler27 <60946932+vsoler27@users.noreply.github.com>
2023-02-01 13:39:46 -0500

    adding routes setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+//importing all of the pages
+import AardvarkGames from './pages/aardvarkgames/aardvarkgames';
+import BoardGame from './pages/aardvarkgames/boardgame';
+import TeamsAndUniversities from './pages/teamsanduniversities/teamsanduniversities';
+import Tournament from './pages/tournament/tournament/tournament';
+import Schedule from './pages/tournament/schedule';
+
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar />
+      <div style={{ margin: 20 }}>
+      <Routes>
+        <Route path="/aardvarkgames" element={<AardvarkGames />} />
+        {/* <Route exact path="/boardgame" element={<BoardGame />} /> */}
+        <Route path="/teamsanduniversities" element={<TeamsAndUniversities />} />
+        <Route exact path="/tournament" element={<Tournament />} />
+        {/* <Route exact path="/schedule" element={<Schedule />} /> */}
+      </Routes>
+      </div>
     </div>
   );
-}
+};
 
 export default App;

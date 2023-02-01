88718ae213b2a318f280d682e93b0e26b00bdaa9
Aaron Putterman <abp6318@rit.edu>
2023-02-01 12:56:30 -0500

    Creating navbar component

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,1 +1,21 @@
 // TODO for Alexis
+
+import React from "react";
+import './navbar.css';
+
+// Here, we display our Navbar
+export default function Navbar() {
+  return (
+    <>
+      <nav>
+        <ul>
+            <li><a href="#TOURNAMENT">LOGO</a></li>
+            <li><a href="#TOURNAMENT">TOURNAMENT</a></li>
+            <li><a href="#TEAMSUNIVERSITIES">TEAMS & UNIVERSITIES</a></li>
+            <li><a href="#AARDVARKGAMES">AARDVARK GAMES</a></li>
+            <li><button>LOGIN</button></li>
+        </ul>
+      </nav>
+    </>
+  );
+}

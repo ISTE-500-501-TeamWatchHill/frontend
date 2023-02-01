3966ee40e0385fe7855757435d745e345113437f
vsoler27 <60946932+vsoler27@users.noreply.github.com>
2023-02-01 13:50:45 -0500

    adding missing imports whoops lol

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
+import React from "react";
 import Navbar from './components/navbar/navbar';
 
+// We use Route in order to define the different routes of our application
+import { Route, Routes } from "react-router-dom";
+
 //importing all of the pages
-import AardvarkGames from './pages/aardvarkgames/aardvarkgames';
-import BoardGame from './pages/aardvarkgames/boardgame';
+import AardvarkGames from './pages/aardvarkgames/aardvarkgames/aardvarkgames';
+// import BoardGame from './pages/aardvarkgames/boardgame/boardgame';
 import TeamsAndUniversities from './pages/teamsanduniversities/teamsanduniversities';
 import Tournament from './pages/tournament/tournament/tournament';
-import Schedule from './pages/tournament/schedule';
+// import Schedule from './pages/tournament/schedule/schedule';
 
 
 const App = () => {

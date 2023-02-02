809055fdf8db1dc0c6ff6b648310d29915bf4b9e
a-a-gordon <agordonpolarbear@gmail.com>
2023-02-02 16:31:56 -0500

    Restructure for teamsanduniversities pages

--- src/App.js
+++ src/App.js
@@ -8,9 +8,10 @@ import { Route, Routes } from "react-router-dom";
 //importing all of the pages
 import AardvarkGames from './pages/aardvarkgames/aardvarkgames/aardvarkgames';
 import BoardGame from './pages/aardvarkgames/boardgame/boardgame';
-import TeamsAndUniversities from './pages/teamsanduniversities/teamsanduniversities';
+import TeamsAndUniversities from './pages/teamsanduniversities/teamsanduniversities/teamsanduniversities';
 import Tournament from './pages/tournament/tournament/tournament';
 import Schedule from './pages/tournament/schedule/schedule';
+import Team from './pages/teamsanduniversities/team/team';
 
 
 const App = () => {
@@ -24,6 +25,7 @@ const App = () => {
         <Route path="/teamsanduniversities" element={<TeamsAndUniversities />} />
         <Route exact path="/tournament" element={<Tournament />} />
         <Route exact path="/schedule" element={<Schedule />} />
+        <Route exact path="/team" element={<Team />} />
       </Routes>
       </div>
     </div>
---
+++ src/pages/teamsanduniversities/team/team.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './team.css';
+import image from '../../../components/placeholder.png';
+
+const Team = (props) => {   
+    return (
+          <>
+            <div>Hello this is a team page</div>
+          </>
+    )
+};
+  
+export default Team;

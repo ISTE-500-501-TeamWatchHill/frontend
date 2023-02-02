98caa6b9b0ffa7a1dc196999c21e27f178988a9e
Aaron Putterman <abp6318@rit.edu>
2023-02-01 22:42:05 -0500

    Steps in the right direction... took me a minute

--- src/components/navlink/navlink.js
+++ src/components/navlink/navlink.js
@@ -1,4 +1,15 @@
+import React, { useState } from 'react';
+import './navlink.css';
+
 export default function NavLink(props) { 
+    const [show, setShow] = useState(false);
+    const showDropdown = (e)=>{
+        setShow(!show);
+    }
+    const hideDropdown = (e) => {
+        setShow(false);
+    }
+
     // check props.name exists
     // check props.link || props.sublinks exists
     if (props.link) {
@@ -14,12 +25,12 @@ export default function NavLink(props) {
     if (props.sublinks) {
         return (
             <>
-                <a href={props.link}>
+                <a href={props.link} class="dropdown">
                     {props.name}
                 </a>
                 <div className="dropdown-items">
                     {props.sublinks.map((sublink) => (
-                        <a href={sublink.link}>{sublink.name}</a>
+                        <a href={sublink.link} key={sublink.link}>{sublink.name}</a>
                     ))}
                 </div>
             </>

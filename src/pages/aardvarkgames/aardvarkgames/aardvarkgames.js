e451f7417696cee54339a0cd231773636f789685
vsoler27 <60946932+vsoler27@users.noreply.github.com>
2023-02-02 14:31:28 -0500

    fixed it, slay

--- src/pages/aardvarkgames/aardvarkgames/aardvarkgames.js
+++ src/pages/aardvarkgames/aardvarkgames/aardvarkgames.js
@@ -4,7 +4,7 @@ const AardvarkGames = () => {
 return (
       <>
         <h1 style={{color:"blue"}}> 
-            Welcome to Aardvark Bitches</h1>
+            Welcome to Aardvark</h1>
       </>
   )
 };

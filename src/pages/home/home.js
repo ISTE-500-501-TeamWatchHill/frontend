a1bd4b5935cb14387be076008716f1340f477e28
Aaron Putterman <abp6318@rit.edu>
2023-02-04 13:50:02 -0500

    Finished section section

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -22,6 +22,18 @@ const Home = () => {
                     <Button name="Register"/>
                 </div>
             </div>
+
+            <div className='second-section'>
+                <h3 className='text'>CAN YOUR UNIVERSITY'S TEAM BRING HOME THE PRIZE?</h3>
+                <Spacer height='32px'/>
+                <div className='text'>
+                    <p className='text'>Gather a team and sign up to play, first for the honor of being your University's championship team and then for the chance to represent your school in continued rounds of global competition.</p>
+                    <Spacer height='40px'/>
+                    <p className='text'>A New World requires a team of 2-5 players who will work together to score as many points as possible after being dropped into a new, unpopulated world. For the tournament, teams will play in a head-to-head competition with an opponent seeking to survive in its own New World, but competing with your team for the same resources.</p>
+                    <Spacer height='40px'/>
+                    <p className='text'>Join us for some great gaming fun! Join us for some awesome tournament prizes!</p>
+                </div>
+            </div>
         </>
     )
 };

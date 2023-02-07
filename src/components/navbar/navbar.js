6b632bbde88217a98b8d1936f25a950d5f84d8a9
Aaron Putterman <abp6318@rit.edu>
2023-02-06 21:36:14 -0500

    Changed favicon, added logo to navbar, changed title

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -2,45 +2,42 @@ import React from "react";
 import './navbar.css';
 import NavLink from '../navlink/navlink';
 import Button from '../button/button';
+import logo from '../Aardvark_logo_clear_horizontal.png';
 
-
-// Here, we display our Navbar
 export default function Navbar() {
   return (
     <>
       <nav>
-        <ul>
-            <li>
-                <NavLink 
-                    name="TOURNAMENT"
-                    sublinks={[ 
-                      { name: "ABOUT", link: "/tournament" },
-                      { name: "SCHEDULE", link: "/schedule" },
-                    ]}
-                />
-            </li>
-            <li>
-                <NavLink 
-                    name="TEAMS & UNIVERSITIES"
-                    link="/teamsanduniversities"
-                />
-            </li>
-            <li>
-                <NavLink 
-                    name="AARDVARK GAMES"
-                    sublinks={[ 
-                      { name: "ABOUT", link: "/aardvarkgames" },
-                      { name: "BOARD GAME", link: "/boardgame" },
-                    ]}
-                />
-            </li>
-            {/* <li>
-              <Button 
-                name="Login"
-              />
-            </li> */}
-        </ul>
-        <Button name="Login"/>
+        <a href='/'><img className='logo' src={logo} alt='Aarvark Games Logo'/></a>
+        <div className='right-nav'>
+          <ul>
+              <li>
+                  <NavLink 
+                      name="TOURNAMENT"
+                      sublinks={[ 
+                        { name: "ABOUT", link: "/tournament" },
+                        { name: "SCHEDULE", link: "/schedule" },
+                      ]}
+                  />
+              </li>
+              <li>
+                  <NavLink 
+                      name="TEAMS & UNIVERSITIES"
+                      link="/teamsanduniversities"
+                  />
+              </li>
+              <li>
+                  <NavLink 
+                      name="AARDVARK GAMES"
+                      sublinks={[ 
+                        { name: "ABOUT", link: "/aardvarkgames" },
+                        { name: "BOARD GAME", link: "/boardgame" },
+                      ]}
+                  />
+              </li>
+          </ul>
+          <Button name="Login"/>
+        </div> {/* classname='right-nav' */}
       </nav>
     </>
   );

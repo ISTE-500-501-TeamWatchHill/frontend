bc56c29c085eb90dafa871cde41c58e48b04f5d4
a-a-gordon <agordonpolarbear@gmail.com>
2023-02-09 14:56:05 -0500

    Base start for team page

--- src/pages/teamsanduniversities/team/team.js
+++ src/pages/teamsanduniversities/team/team.js
@@ -2,14 +2,39 @@ import React from 'react'
 import './team.css';
 import image from '../../../components/placeholder.png';
 import { useParams } from "react-router-dom";
+import MemberBlock from '../../../components/memberblock/memberblock';
+
+
+//Hard coded for now- will grab from database
+const members = [ 
+  { id: 1533, name: "John Smith", image: image },
+  { id: 2354, name: "Jane Doe", image: image },
+  { id: 3876, name: "Cindy Lou", image: image },
+  { id: 4767, name: "Mary", image: image },
+];
 
 const Team = (props) => {   
 
-  let { team } = useParams();
+  let { id } = useParams();
 
     return (
           <>
-            <div>Hello this is a team page for {JSON.parse(team).name}</div>
+            {/* Title */}
+            <h1> Team Name ({id}) </h1>
+
+            <h3> University Name</h3>
+
+            <div class="grid">
+                {/* Team Members */}
+                {
+                    // eslint-disable-next-line
+                    members.map((member) => {
+                        return (
+                            <MemberBlock member={member} />
+                        )
+                    })
+                }
+            </div>
           </>
     )
 };

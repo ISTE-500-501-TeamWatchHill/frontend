e69a5b81433c1e17571ade863f93b1f929fa6537
Aaron Putterman <abp6318@rit.edu>
2023-02-09 14:38:03 -0500

    Buttons are now have dynamic width and background properties

--- src/components/button/button.js
+++ src/components/button/button.js
@@ -1,33 +1,39 @@
 import React, { useState } from "react";
 import styles from './button.module.css';
-import _uniqueId from 'lodash/uniqueId';
 
 // TODO: Handle icons in buttons? (like on logged in profile page)
 // name: String                     -- string included within the button
 // width: Int                       -- width of button (subtract width of word and 34px of padding)
 // transparentBackground: Boolean   -- background of button is clear
 export default function Button(props) {
-    const [uid] = useState(_uniqueId('button-uid-'));
     
-    // name
     if (!props.name) {
         throw new Error ("ERROR: No name provided for button component.");
     }
 
-    // width has a default
+    // styles passed in with the props
+    let propStyles = {};
+
+    // width={'150px'} OR width={'50%'} OR ...
     if (props.width) {
-        // TODO: Set width dynamically
+        // set width dynamically if passed in
+        propStyles.width = props.width;
+    } else {
+        // else set default padding
+        propStyles.padding = `12px 17px`;
     }
 
-    // background has a default
+    // transparentBackground={true}
     if (props.transparentBackground) {
-        // Set background to transparent
-        console.log(document.getElementsByClassName(uid));
+        // set background to transparent and change color of text
+        propStyles.backgroundColor = `transparent`;
+        propStyles.color = `#525252`;
+        propStyles.border = `1px solid #525252`;
     }
 
     return (
         <>
-            <button className={`${styles.button} ${uid}`}>
+            <button className={styles.button} style={propStyles}>
                 {props.name}
             </button>
         </>
--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -36,7 +36,7 @@ export default function Navbar() {
                   />
               </li>
           </ul>
-          <Button name="Login" transparentBackground={true}/>
+          <Button name="Login"/>
         </div> {/* classname='right-nav' */}
       </nav>
     </>

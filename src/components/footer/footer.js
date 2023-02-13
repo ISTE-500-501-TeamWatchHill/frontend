a01684d1b132ae572ddefa5d280d56efb23aa094
Aaron Putterman <abp6318@rit.edu>
2023-02-13 13:30:01 -0500

    Updated footer css

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,17 +1,17 @@
 import React from "react";
 import logo from '../Aardvark_logo_clear.png';
-import './footer.css';
+import styles from './footer.module.css';
 
 export default function Footer(props) { 
     return (
         <>
-            <footer>
-                <a href='/'><img className='logo' src={logo} alt='Aarvark Games Logo'/></a>
+            <footer className={styles.footer}>
+                <a href='/'><img className={styles.logo} src={logo} alt='Aarvark Games Logo'/></a>
                 <div className='footerList'>
-                    <div className='footerListHeader'>NAVIGATION</div>
-                    <div className='footerListItem'><a href='/tournament'>TOURNAMENT</a></div>
-                    <div className='footerListItem'><a href='/teamsanduniversities'>TEAMS & UNIVERSITIES</a></div>
-                    <div className='footerListItem'><a href='/aardvarkgames'>AARDVARK GAMES</a></div>
+                    <div className={styles.footerListHeader}>NAVIGATION</div>
+                    <div className={styles.footerListItem}><a href='/tournament'>TOURNAMENT</a></div>
+                    <div className={styles.footerListItem}><a href='/teamsanduniversities'>TEAMS & UNIVERSITIES</a></div>
+                    <div className={styles.footerListItem}><a href='/aardvarkgames'>AARDVARK GAMES</a></div>
                 </div>
             </footer>
         </>

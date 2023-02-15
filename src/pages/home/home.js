21202e95de12de37c79605ffc383b0d62461b2b7
a-a-gordon <agordonpolarbear@gmail.com>
2023-02-15 13:45:33 -0500

    Add navigation to learn more button on home page

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -29,7 +29,7 @@ const Home = () => {
                 <div className='countdown'>TODO: Dynamic countdown timer here</div>
                 <Spacer height='40px' />
                 <div className='button-row'>
-                    <Button name="Learn More" transparentBackground={true}/>
+                    <Button name="Learn More" transparentBackground={true} link="/tournament"/>
                     <Spacer width="20px"/>
                     <Button name="Register for Tournament" link="/register"/>
                 </div>

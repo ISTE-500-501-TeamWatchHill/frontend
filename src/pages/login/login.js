307fe2994d1f32297a12ab83ba3b22f300e2e54a
Aaron Putterman <abp6318@rit.edu>
2023-02-16 14:48:07 -0500

    Console errors fixed for login and registration

--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -1,25 +1,25 @@
 import React from 'react'
-import './login.css';
+// import styles from './login.module.css';
 
 const Login = (props) => {   
     return (
           <>
-            <h1 class="centerItem"> Login </h1>
-            <h3 class="centerItem"> Log into your account! </h3>
-            <div class="center">
+            <h1 className="centerItem"> Login </h1>
+            <h3 className="centerItem"> Log into your account! </h3>
+            <div className="center">
                 <br/><br/>
-                <form action="/login" method="get">
-                    <label for="fname">University Email:</label><br/>
-                    <input type="text" id="email" name="email" value=""></input><br/><br/>
+                <form action="/login" method="POST">
+                    <label htmlFor="fname">University Email:</label><br/>
+                    <input type="text" id="email" name="email"></input><br/><br/>
 
-                    <label for="fname">Password:</label><br/>
-                    <input type="text" id="password" name="password" value=""></input><br/><br/>
+                    <label htmlFor="fname">Password:</label><br/>
+                    <input type="text" id="password" name="password"></input><br/><br/>
 
                     <input type="submit" value="Submit"></input>
                 </form>
             </div>
             <br/>
-            <h3 class="centerItem"> Not registered for the tournament yet? <a href="/register">Register here</a></h3>
+            <h3 className="centerItem"> Not registered for the tournament yet? <a href="/register">Register here</a></h3>
           </>
     )
 };

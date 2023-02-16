6889c009001eac3c60f4e2fab99c80745c592d2b
Aaron Putterman <abp6318@rit.edu>
2023-02-16 14:29:31 -0500

    more stuff converted

--- src/pages/aardvarkgames/aardvarkgames/aardvarkgames.js
+++ src/pages/aardvarkgames/aardvarkgames/aardvarkgames.js
@@ -12,13 +12,13 @@ return (
           <h1 className={globalStyles.h1_title}>Aardvark Games</h1>
         </div>
 
-        <div className={globalStyles.second_section}>
+        <div className={globalStyles.subsection}>
           <h3 className={globalStyles.text}>ABOUT</h3>
           <Spacer height='32px' />
           <p className={`${globalStyles.text} ${globalStyles.p}`}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce lobortis mollis varius. In a rhoncus ex. Vivamus mollis est quis molestie accumsan. Suspendisse gravida, dui non pharetra posuere, tortor ex scelerisque est, id fermentum ligula elit sit amet urna.</p>
         </div>
 
-        <div className={`${globalStyles.second_section}`}>
+        <div className={`${globalStyles.subsection}`}>
           <h3 className={globalStyles.text}>PRODUCTS</h3>
           <Spacer height='32px' />
           <p className={`${globalStyles.text} ${globalStyles.p}`}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce lobortis mollis varius. In a rhoncus ex. Vivamus mollis est quis molestie accumsan. Suspendisse gravida, dui non pharetra posuere, tortor ex scelerisque est, id fermentum ligula elit sit amet urna.</p>
--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styles from './home.module.css';
+import globalStyles from '../pages.module.css';
 import Button from '../../components/button/button';
 import Spacer from '../../components/spacer/spacer';
 
@@ -19,7 +20,7 @@ const Home = () => {
             <div className={styles.home_title_section}>
                 <h1 className={`${styles.home_title} ${styles.h1}`}>A New World</h1>
                 <Spacer height='16px' />
-                <div className={styles.text}>Introducing Aardvark's newest board game, A New World, with a global collegiate competition!</div>
+                <div className={globalStyles.text}>Introducing Aardvark's newest board game, A New World, with a global collegiate competition!</div>
                 <Spacer height='42px' />
                 <div className={styles.countdown}>TODO: Dynamic countdown timer here</div>
                 <Spacer height='40px' />
@@ -30,26 +31,26 @@ const Home = () => {
                 </div>
             </div>
 
-            <div className={styles.second_section}>
-                <h3 className={styles.text}>CAN YOUR UNIVERSITY'S TEAM BRING HOME THE PRIZE?</h3>
+            <div className={globalStyles.subsection}>
+                <h3 className={globalStyles.text}>CAN YOUR UNIVERSITY'S TEAM BRING HOME THE PRIZE?</h3>
                 <Spacer height='32px'/>
-                <div className={styles.text}>
-                    <p className={`${styles.p} ${styles.text}`}>Gather a team and sign up to play, first for the honor of being your University's championship team and then for the chance to represent your school in continued rounds of global competition.</p>
+                <div className={globalStyles.text}>
+                    <p className={`${globalStyles.p} ${globalStyles.text}`}>Gather a team and sign up to play, first for the honor of being your University's championship team and then for the chance to represent your school in continued rounds of global competition.</p>
                     <Spacer height='40px'/>
-                    <p className={`${styles.p} ${styles.text}`}>A New World requires a team of 2-5 players who will work together to score as many points as possible after being dropped into a new, unpopulated world. For the tournament, teams will play in a head-to-head competition with an opponent seeking to survive in its own New World, but competing with your team for the same resources.</p>
+                    <p className={`${globalStyles.p} ${globalStyles.text}`}>A New World requires a team of 2-5 players who will work together to score as many points as possible after being dropped into a new, unpopulated world. For the tournament, teams will play in a head-to-head competition with an opponent seeking to survive in its own New World, but competing with your team for the same resources.</p>
                     <Spacer height='40px'/>
-                    <p className={`${styles.p} ${styles.text}`}>Join us for some great gaming fun! Join us for some awesome tournament prizes!</p>
+                    <p className={`${globalStyles.p} ${globalStyles.text}`}>Join us for some great gaming fun! Join us for some awesome tournament prizes!</p>
                 </div>
             </div>
 
             <div className={styles.third_section}>
                 <img src='https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=Aardvark%20Games' alt='TODO'/>
                 <Spacer width='75px'/>
-                <p className={`${styles.p} ${styles.text}`}>All players who complete at least one round of tournament play will receive a complimentary copy of A New World. Each university's final round teams will go home with some awesome Aardvark Games swag. The First Place team for each university will receive a cash prize of $1,000 and each individual team member will get a $100 gift certificate for the Aardvark Games online store.</p>
+                <p className={`${globalStyles.p} ${globalStyles.text}`}>All players who complete at least one round of tournament play will receive a complimentary copy of A New World. Each university's final round teams will go home with some awesome Aardvark Games swag. The First Place team for each university will receive a cash prize of $1,000 and each individual team member will get a $100 gift certificate for the Aardvark Games online store.</p>
             </div>
 
             <div className={styles.fourth_section}>
-                <h3 className={styles.text}>WHO'S PLAYING?</h3>
+                <h3 className={globalStyles.text}>WHO'S PLAYING?</h3>
                 <Spacer height='48px' />
                 <div className={styles.teams_playing}>
                     <div className={styles.team_playing}>

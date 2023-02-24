47b794352c62901b538ea3d2e040eeb22c23a9cb
vsoler27 <60946932+vsoler27@users.noreply.github.com>
2023-02-24 16:35:38 -0500

    adding in temp changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import React from "react";
+import React, {Component} from "react";
 import Navbar from './components/navbar/navbar';
 import Footer from './components/footer/footer';
 
@@ -18,24 +18,44 @@ import Registration from './pages/registration/registration';
 import Login from './pages/login/login';
 
 
-const App = () => {
-  return (
-    <div>
-      <Navbar />
-      <Routes>
-        <Route path="/aardvarkgames" element={<AardvarkGames />} />
-        <Route exact path="/boardgame" element={<BoardGame />} />
-        <Route path="/teamsanduniversities" element={<TeamsAndUniversities />} />
-        <Route exact path="/tournament" element={<Tournament />} />
-        <Route exact path="/schedule" element={<Schedule />} />
-        <Route exact path="/team" element={<Team />} />
-        <Route exact path="/" element={<Home />} />
-        <Route exact path="/register" element={<Registration />} />
-        <Route path="/login" element={<Login />} />
-      </Routes>
-      <Footer />
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {apiResponse: ""};
+  }
+
+  callAPI() {
+    fetch("http://localhost:8080/teams")
+      .then(res => res.text())
+      .then(res => this.setState({ apiRespone: res}))
+      .catch(err => err);
+  }
+  
+
+  componentDidMount() {
+    this.callAPI();
+  }
+
+  render() {
+    return (
+      <div>
+        <Navbar />
+        <h1>{this.state.apiRespone}</h1>
+        <Routes>
+          <Route path="/aardvarkgames" element={<AardvarkGames />} />
+          <Route exact path="/boardgame" element={<BoardGame />} />
+          <Route path="/teamsanduniversities" element={<TeamsAndUniversities/>} />
+          <Route exact path="/tournament" element={<Tournament />} />
+          <Route exact path="/schedule" element={<Schedule />} />
+          <Route exact path="/team" element={<Team />} />
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/register" element={<Registration />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+        <Footer />
+      </div>
+    );
+  }
 };
 
 export default App;

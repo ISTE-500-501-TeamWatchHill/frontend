a56f257ad75204ee787704344c23ea774687810f
Aaron Putterman <abp6318@rit.edu>
2023-02-26 18:44:04 -0500

    Switched var to const

--- src/pages/teamsanduniversities/university/university.js
+++ src/pages/teamsanduniversities/university/university.js
@@ -10,7 +10,7 @@ import TeamBlock from '../../../components/teamblock/teamblock';
 
 
 //Hard coded for now- will grab from database.
-var teams = [ 
+const teams = [ 
   { id: 1, name: "Team One", universityname: "Rochester Istitute of Technology", universityid: 1, numplayers: 4 },
   { id: 2, name: "Team Two", universityname: "Rochester Istitute of Technology", universityid: 1, numplayers: 4 },
   { id: 3, name: "Team Three", universityname: "Rochester Istitute of Technology", universityid: 1, numplayers: 5 },

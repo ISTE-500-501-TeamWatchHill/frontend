00aee9ee1c09a0d7343efe66e4ab10f58857068f
a-a-gordon <agordonpolarbear@gmail.com>
2023-02-26 10:58:59 -0500

    Creating a reusable header component

---
+++ src/components/header/header.js
@@ -0,0 +1,17 @@
+import React from "react";
+import styles from './header.module.css';
+import globalStyles from '../../pages/pages.module.css';
+
+export default function Header(props) { 
+    if (!props.name) {
+        throw new Error ("ERROR: No name provided for header component.");
+    }
+
+    return (
+        <>
+            <div className={globalStyles.h1_title_section}>
+                <h1 className={globalStyles.h1_title}>{props.name}</h1>
+            </div>
+        </>
+    );
+}

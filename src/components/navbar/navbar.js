54ac1e8695dbf1615378ff6b20d584362b1fa3df
a-a-gordon <agordonpolarbear@gmail.com>
2023-03-15 09:30:01 -0400

    Moving language selector from navigation to footer

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -5,7 +5,6 @@ import NavLink from '../navlink/navlink';
 import Button from '../button/button';
 import logo from '../Aardvark_logo_clear_horizontal.png';
 import { useTranslation } from "react-i18next";
-import LanguageSelector from "../languageselector/languageselector";
 import Cookies from 'universal-cookie';
 
 export default function Navbar() {
@@ -49,7 +48,6 @@ export default function Navbar() {
           { !user && <Button name={t("loginButton.label")} link="/login"/> }
           { user && <Button name="Logout" link="/login" onClick={function() {cookies.remove('user');}}/> }
           { user && <div>Welcome, { user.firstName }!</div>}
-          <LanguageSelector />
         </div> {/* classname='right_nav' */}
       </nav>
     </>

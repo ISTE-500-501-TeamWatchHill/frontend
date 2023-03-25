55b3b683b918d7c26e476c63f87e302987a0b40c
vsoler27 <60946932+vsoler27@users.noreply.github.com>
2023-03-25 12:03:14 -0400

    hardcoded profile info and set up useEffect while waiting for endpoint

--- src/pages/profile/profile.js
+++ src/pages/profile/profile.js
@@ -1,8 +1,7 @@
 import React, {useState, useEffect} from 'react'
-import Select from 'react-select'
 import globalStyles from '../pages.module.css';
 import styles from './profile.module.css';
-import { Navigate, useNavigate } from "react-router-dom";
+import { Navigate } from "react-router-dom";
 import Cookies from 'universal-cookie';
 import Header from '../../components/header/header';
 
@@ -13,17 +12,53 @@ const Profile = (props) => {
     const BASE_URL = process.env.REACT_APP_BASE_URL;
     const cookies = new Cookies();
     const user = cookies.get('user');
+    // eslint-disable-next-line
+    let myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+    const [person, setPerson] = useState({
+      //"_id": ObjectId("640f5aa209be69cb0b64e42d"),
+      "uid": 1423518,
+      "roleID": 19202,
+      "universityID": 2760,
+      "firstName": "Test",
+      "lastName": "User",
+      "teamName": "Sample Team One",
+      "universityName": "RIT",
+      "email": "spammewemails@rit.edu"
+    });
 
     /* TODO:
-     * change the Welcome in nav to image
+     * dynamic profile pic
         * need endpoint for this also? 
-        * use dummy for now 
-     * finish adding in all of the other stuff that's supposed to go on this page as dummy data
      * connect to backend
         * what endpoint will allow me to get user info based on whats in the cookie? 
         * /how do i get this info 
-     * style it  
+     * style it - alexis pls <3
     */
+
+    useEffect(()=> {
+      async function getUser() {
+          const raw = JSON.stringify({
+            "email": user.email
+          });
+  
+          const requestOptions = {
+              method: 'POST',
+              headers: myHeaders,
+              body: raw,
+          };
+  
+          await fetch(`${BASE_URL}/userPub/byID`, requestOptions)
+              .then(response => response.json())
+              .then(function(result) {
+                setPerson(result); 
+              })
+              .catch(function(error) {
+                  console.log('error', error);
+              });
+      }
+      //getUser();
+    },[BASE_URL, user, myHeaders])
     
 
     return (
@@ -35,6 +70,11 @@ const Profile = (props) => {
             <Header 
               name={`${user.firstName} ${user.lastName}`}
             />
+            <p className={`${globalStyles.text} ${globalStyles.wide_p} ${globalStyles.white}`}>Name: {`${user.firstName} ${user.lastName}`}</p>
+            <p className={`${globalStyles.text} ${globalStyles.wide_p} ${globalStyles.white}`}>Password: ****</p>
+            <p className={`${globalStyles.text} ${globalStyles.wide_p} ${globalStyles.white}`}>University: {`${person.universityName}`}</p>
+            <p className={`${globalStyles.text} ${globalStyles.wide_p} ${globalStyles.white}`}>University Email: {`${person.email}`}</p>
+            <p className={`${globalStyles.text} ${globalStyles.wide_p} ${globalStyles.white}`}>Team: {`${person.teamName}`}</p>
           </div>
         </>
   )

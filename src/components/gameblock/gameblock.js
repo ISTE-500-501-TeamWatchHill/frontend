9f5b4763cc8e6cc45cfd634a1a2aefd117b36132
cl1252 <47199057+cl1252@users.noreply.github.com>
2023-04-19 18:59:30 -0400

    #11 updated gameblock styling for schedule (values are hardcoded instead of pulling from database)

--- src/components/gameblock/gameblock.js
+++ src/components/gameblock/gameblock.js
@@ -2,7 +2,8 @@ import React from 'react';
 import styles from './gameblock.module.css';
 import globalStyles from '../../pages/pages.module.css';
 
-import image from '../placeholder.png';
+import image from '../../../src/assets/images/universities/rit_circle.png';
+import image2 from '../../../src/assets/images/universities/cornell_circle.png';
 
 const GameBlock = (props) => {
     if (!props.game._id) {
@@ -34,31 +35,35 @@ const GameBlock = (props) => {
             <div className={styles.block}>
                 <div className={`${styles.flexBlock} ${styles.center}`}>
                     <div>
-                        <p><strong>START TIME</strong></p>
-                        <p>{props.game.gameTime}</p>
+                        <p className={styles.h1}>START TIME</p>
+                        <p className={styles.h2}>{props.game.gameTime}</p>
                         <br/>
-                        <p><strong>LOCATION</strong></p>
-                        <p>{props.game.locationInfo[0].name}</p>
+                        <p className={styles.h1}>LOCATION</p>
+                        <p className={styles.h2}>{props.game.locationInfo[0].name}</p>
                     </div>
 
                     <div className={`${styles.flex} ${styles.center}`}>
                         <div>
                             {/* Need to fix hard coded url- need team IDs in addition to names or a way to get team name by ID */}
+                            <p  className={styles.h1}>Team</p>
                             <a href={"/team/1"} key={"/team/2"} className={globalStyles.text}>{props.game.homeTeamInfo[0].description}</a>
-                            <p>Home Team University</p>
+                            <p className={styles.h2}>Team Name</p>
+                            <p>Rochester Institute of Technology</p>
                         </div>
 
                         <img className={styles.img} src={image} alt="Placeholder"/>
 
                         <div>
-                            <p className={globalStyles.text}>[]</p>
+                            <p className={globalStyles.text}> VS </p>
                         </div>
 
-                        <img className={styles.img} src={image} alt="Placeholder"/>
+                        <img className={styles.img} src={image2} alt="Placeholder"/>
 
                         <div>
+                            <p className={styles.h1}>Team</p>
                             <a href={"/team/1"} key={"/team/1"}className={globalStyles.text}>{props.game.awayTeamInfo[0].description}</a>
-                            <p>Away Team University</p>
+                            <p className={styles.h2}>Team Name</p>
+                            <p>Cornell University</p>
                         </div>
                     </div>
                 </div>

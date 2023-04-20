cf9f6c33c02dcba597bdeffc185b7d49c389af26
Aaron Putterman <abp6318@rit.edu>
2023-04-20 11:59:54 -0400

    Found and fixed issue

--- src/components/teamblock/teamblock.js
+++ src/components/teamblock/teamblock.js
@@ -2,9 +2,9 @@ import React from 'react'
 import styles from './teamblock.module.css';
 import image from '../placeholder.png';
 import globalStyles from '../../pages/pages.module.css';
-// import Team from '../../pages/teamsanduniversities/team/team';
 
 const TeamBlock = (props) => { 
+    console.log(props);
     return (
           <>
             <div className={styles.block}>
@@ -13,7 +13,7 @@ const TeamBlock = (props) => {
 
                     <div>
                         <a className={globalStyles.text} href={"/team/" + props.team._id} key={"/team/" + props.team._id} >{props.team.description}</a>
-                        <p className={globalStyles.sub_text}><a href={"/university/" + props.team.universityInfo[0].universityID} key={"/university/" + props.team.universityInfo[0].universityID}>{props.team.universityInfo[0].name}</a></p>
+                        {props.team.universityInfo && <p className={globalStyles.sub_text}><a href={"/university/" + props.team.universityInfo[0].universityID} key={"/university/" + props.team.universityInfo[0].universityID}>{props.team.universityInfo[0].name}</a></p>}
                         <p className={globalStyles.sub_text}>{props.team.players.length} PLAYERS</p>
                     </div>
                 </div>

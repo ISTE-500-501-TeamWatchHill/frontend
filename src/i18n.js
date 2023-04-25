478c47402bddcc9f6adff2a53ec3537cfba6d91f
a-a-gordon <agordonpolarbear@gmail.com>
2023-04-25 16:36:37 -0400

    Fixing i18next to preserve language in ls

--- src/i18n.js
+++ src/i18n.js
@@ -1,5 +1,6 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
 
 
 // Importing translation files
@@ -18,14 +19,19 @@ const resources = {
   },
 };
 
-//i18N Initialization
+const DETECTION_OPTIONS = {
+  order: ['localStorage', 'navigator'],
+  caches: ['localStorage']
+};
 
+//i18N Initialization
 i18n
+  .use(LanguageDetector)
   .use(initReactI18next)
   .init({
     resources,
-    lng:"en", //default language
-    keySeparator: false,
+    detection: DETECTION_OPTIONS,
+    fallbackLng: "en",
     interpolation: {
       escapeValue: false,
     },

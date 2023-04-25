a2b166c3192d0ce3e8ce81f8e6175507356a62f7
a-a-gordon <agordonpolarbear@gmail.com>
2023-04-25 16:35:42 -0400

    Prepping translation files for changes

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -13,16 +13,16 @@ export default function Footer(props) {
             <footer className={styles.footer}>
                 <div className={styles.footerLeftBlock}>
                     <h4 className={styles.footerTitle}>A New World</h4>
-                    <div className={styles.subcontent}>{t("footerSubtitle.label")}</div>
+                    <div className={styles.subcontent}>{t("footerSubtitle.home")}</div>
                     <p>Select your language: <LanguageSelector /></p>
                     <Spacer height='119px' />
                     <div className={styles.subcontent}>&copy; Copyright 2023 · Team Watch Hill</div>
                 </div>
                 <div className={styles.footerList}>
-                    <div className={styles.footerListHeader}>{t("navigation.label")}</div>
-                    <div className={styles.footerListItem}><a href='/tournament'>{t("tournamentNav.label")}</a></div>
-                    <div className={styles.footerListItem}><a href='/teamsanduniversities'>{t("teamsanduniversitiesNav.label")}</a></div>
-                    <div className={styles.footerListItem}><a href='/aardvarkgames'>{t("aardvarkGamesNav.label")}</a></div>
+                    <div className={styles.footerListHeader}>{t("navigation.home")}</div>
+                    <div className={styles.footerListItem}><a href='/tournament'>{t("tournamentNav.home")}</a></div>
+                    <div className={styles.footerListItem}><a href='/teamsanduniversities'>{t("teamsanduniversitiesNav.home")}</a></div>
+                    <div className={styles.footerListItem}><a href='/aardvarkgames'>{t("aardvarkGamesNav.home")}</a></div>
                 </div>
             </footer>
         </>

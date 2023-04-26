19d8c1447cf56b82c632bb8612701035bd2181b3
a-a-gordon <agordonpolarbear@gmail.com>
2023-04-25 20:11:48 -0400

    about tournament page

--- src/pages/tournament/tournament/tournament.js
+++ src/pages/tournament/tournament/tournament.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useTranslation } from "react-i18next";
 import styles from './tournament.module.css';
 import globalStyles from '../../pages.module.css';
 import "@fontsource/mulish";
@@ -7,45 +8,46 @@ import Spacer from '../../../components/spacer/spacer';
 import Button from '../../../components/button/button';
 
 const Tournament = () => {
+  const { t } = useTranslation();
     
 return (
       <>
         <div className={`${globalStyles.h1_title_section} ${styles.background}`}>
-            <h1 className={globalStyles.h1_title}>Tournament</h1>
+            <h1 className={globalStyles.h1_title}>{t("tournamentAboutTitle.tournament")}</h1>
         </div>
 
         <div className={`${globalStyles.body_margin} ${globalStyles.margin8_top_bottom}`}>
-          <h3 className={`${globalStyles.headline_text}`}>Rules</h3>
+          <h3 className={`${globalStyles.headline_text}`}>{t("tournamentAboutRulesTitle.tournament")}</h3>
 
           <p className={`${globalStyles.green_bar} ${globalStyles.sub_header_spacer}`}>__</p>
 
-          <p className={`${globalStyles.text} ${globalStyles.sub_text_spacer}`}>Registration for this tournament is limited to countries in which participation is legal. If there is a difference of opinion in interpretation of the law, Aardvark Games' legal counsel will have the final word on a Team's ability to register.</p>
+          <p className={`${globalStyles.text} ${globalStyles.sub_text_spacer}`}>{t("aboutRuleOne.tournament")}</p>
 
-          <p className={`${globalStyles.text} ${globalStyles.sub_text_spacer}`}>Each Team will have at least two, but no more than five, members.</p>
+          <p className={`${globalStyles.text} ${globalStyles.sub_text_spacer}`}>{t("aboutRuleTwo.tournament")}</p>
 
-          <p className={`${globalStyles.text} ${globalStyles.sub_text_spacer}`}>The deadline for registration is midnight EDT on <strong>Monday, May 1, 2023</strong>, unless an extension for all is publicized on the tournament website.</p>
+          <p className={`${globalStyles.text} ${globalStyles.sub_text_spacer}`}>{t("aboutRuleThree.tournament")}</p>
 
-          <p className={`${globalStyles.text} ${globalStyles.sub_text_spacer}`}>Registration must be completed on the tournament website. </p>
+          <p className={`${globalStyles.text} ${globalStyles.sub_text_spacer}`}>{t("aboutRuleFour.tournament")}</p>
 
-          <p className={`${globalStyles.text} ${globalStyles.sub_text_spacer}`}>Each Team member must be currently enrolled at the college/university that the Team wishes to represent. A Team member may not play for more than one college in the 2023 tournament. Eligibility will be verified in advance with the college and team  members will be required to present valid student IDs on the day of the on-site tournament. Any questions regarding IDs on the day of the tournament will be decided by the on-site Moderator representing the college.</p>
+          <p className={`${globalStyles.text} ${globalStyles.sub_text_spacer}`}>{t("aboutRuleFive.tournament")}</p>
 
-          <p className={`${globalStyles.text} ${globalStyles.sub_text_spacer}`}>Any Team that fails to appear in person and on time on the day of the on-campus tournament round forfeits that game.</p>
+          <p className={`${globalStyles.text} ${globalStyles.sub_text_spacer}`}>{t("aboutRuleSix.tournament")}</p>
 
-          <p className={`${globalStyles.text} ${globalStyles.sub_text_spacer}`}>The dates of tournament game play and all Team match ups will be selected by Aardvark Games. If an odd number of teams register for any site, the first team to complete registration will be awarded a bye in the first round of competition. </p>
+          <p className={`${globalStyles.text} ${globalStyles.sub_text_spacer}`}>{t("aboutRuleSeven.tournament")}</p>
 
-          <p className={`${globalStyles.text} ${globalStyles.sub_text_spacer}`}>Game play and scoring will take place according to the printed rules shipped with the game.</p>
+          <p className={`${globalStyles.text} ${globalStyles.sub_text_spacer}`}>{t("aboutRuleEight.tournament")}</p>
 
-          <p className={`${globalStyles.text} ${globalStyles.sub_text_spacer}`}>Tournament play will be run consistently at each location to ensure that all players are treated equally. Both players and moderators are expected to cooperate to run an orderly competition. Players and moderators must treat each other in a fair and respectful manner, following both the rules and the spirit in which those rules were created.</p>
+          <p className={`${globalStyles.text} ${globalStyles.sub_text_spacer}`}>{t("aboutRuleNine.tournament")}</p>
 
-          <p className={`${globalStyles.text} ${globalStyles.sub_text_spacer}`}>Aardvark Games reserves the right to alter these rules, as well as the right to interpret, modify, clarify, or otherwise issue official changes to these rules without prior notice.</p>
+          <p className={`${globalStyles.text} ${globalStyles.sub_text_spacer}`}>{t("aboutRuleTen.tournament")}</p>
         </div>
 
         <div className={styles.sign_up_section}>
-          <h3 className={`${globalStyles.headline_text} ${globalStyles.white} ${globalStyles.sub_header_spacer}`}>Take Home the Prize!</h3>
+          <h3 className={`${globalStyles.headline_text} ${globalStyles.white} ${globalStyles.sub_header_spacer}`}>{t("aboutPrize.tournament")}</h3>
           <div className={globalStyles.button_row}>
-            <Button name="Login" link="/login"/>
+            <Button name={t("loginButton.home")} link="/login"/>
             <Spacer width="20px"/>
-            <Button name="Register for Tournament" link="/register"/>
+            <Button name={t("aboutRegister.tournament")} link="/register"/>
           </div>
         </div>
       </>
